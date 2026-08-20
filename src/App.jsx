48b68eebb36f4f709fe107394fda91d9fee3087e
Natasha Ramburrun <r_natasha100@hotmail.com>
2026-08-20 14:06:16 +0100

    added react and vite packages to the project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function App() {
+    return (
+      <body>
+        We're coming soon Hello fellow shoppers! We're currently building our
+        new fashion store. Add your email below to stay up-to-date with
+        announcements and our launch deals. Email Address
+        <footer class="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Your Name Here</a>.
+        </footer>
+      </body>
+    );
+}
+export default App;
